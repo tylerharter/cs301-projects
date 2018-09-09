be9ab2af5dad2cf994223842bc83b1ead2a25981
tylerharter <tylerharter@gmail.com>
2018-09-09 11:06:26 -0500

    basic test infra

---
+++ fall18/p1/main.py
@@ -0,0 +1,100 @@
+# This line is a comment because it starts with a pound sign (#). That
+# means Python ignores it.  A comment is just for a human reading the
+# code.  This project involve 20 small problems to give you practice
+# with operators, types, and boolean logic.  We'll give you directions
+# (as comments) on what to do for each problem.
+
+############################################################
+print('Problem 1')
+
+# we did this one for you!
+
+print('one plus one is:')
+print(1+1)
+print()
+
+############################################################
+print('Problem 2')
+
+# now fix this one so it prints 4 by adding 2 with 2.  If you just
+# print 4, you may pass the tests and initially get a good grade
+# automatically, but we'll take away those points later when a TA
+# reads your solution!  (this example about grading applies
+# generally).
+
+print('two plus two is:')
+print(0)
+print()
+
+############################################################
+print('Problem 3')
+
+# we did this one for you too!
+
+print('the type of 5 is a(n):')
+print(type(5))
+print()
+
+############################################################
+print('Problem 4')
+
+print('the type of 1.5 is a(n):')
+print() # put something in the parentheses here to get an answer
+print()
+
+############################################################
+print('Problem 5')
+
+print('the type of 5.0 is a(n):')
+print() # put something in the parentheses here to get an answer
+print()
+
+############################################################
+print('Problem 6')
+
+print('the type of "5.0" is a(n):')
+print() # put something in the parentheses here to get an answer
+print()
+
+############################################################
+print('Problem 7')
+
+############################################################
+print('Problem 8')
+
+############################################################
+print('Problem 9')
+
+############################################################
+print('Problem 10')
+
+############################################################
+print('Problem 11')
+
+############################################################
+print('Problem 12')
+
+############################################################
+print('Problem 13')
+
+############################################################
+print('Problem 14')
+
+############################################################
+print('Problem 15')
+
+############################################################
+print('Problem 16')
+
+############################################################
+print('Problem 17')
+
+############################################################
+print('Problem 18')
+
+############################################################
+print('Problem 19')
+
+############################################################
+print('Problem 20')
+
