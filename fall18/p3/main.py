6f0f6a90d2c2d21143b02a8a64ed05e72afb63de
Bob Effinger <Regniffe3@gmail.com>
2018-09-24 17:10:45 -0500

    p3 files

---
+++ fall18/p3/main.py
@@ -0,0 +1,76 @@
+import project 
+
+#Function 1
+#testGetNumRecords()
+
+
+#Function 2
+#testGetName(index)
+
+
+#Function 3
+#testGetWindSpeed(index)
+
+#Function 4
+#searchHurricane(name)
+
+
+#Function 5
+#searchHurricaneByLocation(lat, long)
+
+
+
+#Function 6
+#countHurricane(oceanName)       
+        
+
+#Function 7
+#maxWindSpeedHurricane()
+
+#Function 8 
+#minWindSpeedHurricane()
+
+def main():
+    print("Problem 1")
+    print("The number of records in the DataSet is ")
+    print(0)
+    print()
+    
+    print("Problem 2")
+    print("The name of the hurricane in the 20th row of the dataset is ")
+    print(0)
+    print()
+    
+    print("Problem 3")
+    print("Does the hurricane 'BOB' Exist in the Dataset?")
+    print(True)
+    print()
+    
+    print("Problem 4")
+    print("Does the hurricane 'BRITNEY' Exist in the Dataset?")
+    print(0)
+    print()
+    
+    print("Problem 5")
+    print("Find the hurricane that came at 9.0N Latitude and 157.0W Longitude")
+    print(0)
+    print()
+    
+    print("Problem 6")
+    print("How many hurricanes came from the Pacific?")
+    print(0)
+    print()
+    
+    print("Problem 7")
+    print("How many hurricanes came from the Atlantic?")
+    print(0)
+    print()
+    
+    print("Problem 8")
+    print("What is the difference between the max Windspeed and min Windspeed of the dataSet?")
+    print(0)
+    print()
+if __name__ == '__main__':
+    main()    
+
+
