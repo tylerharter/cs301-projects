0ed58c055d442246f8ce0d833819f92d38629517
tylerharter <tylerharter@gmail.com>
2018-09-26 17:40:18 -0500

    remove framework from test.py so students can better understand the code

--- fall18/p3/test.py
+++ fall18/p3/test.py
@@ -1,50 +1,206 @@
 #!/usr/bin/python
 import subprocess, json, re, sys, importlib
 from inspect import getmembers, isfunction
-#import main 
 
 PASS = 'PASS'
 EPSILON = 0.0001
 
-TESTS_FUNC = [
-    ("testGetNumRecords", [], 529), #1
-    ("testGetName", [24], 'ARLENE'), #2
-    ("testGetWindSpeed", [38], 25), #3
-    ("searchHurricane", ["EMILY"], True), #4
-    ("searchHurricane", ["Random1"], False), #5
-    ("searchHurricane", ["KENNETH"], True), #6
-    ("searchHurricane", ["BORA"], False), #7
-    ("searchHurricaneByLocation", ["12.9N","20.5W"], "FAITH"), #8
-    ("searchHurricaneByLocation", ["11.2N","95.0W" ], "HENRIETTE"), #9
-    ("countHurricane", ["Atlantic"], 288), #10
-    ("countHurricane", ["Pacific"], 241), #11
-    ("maxWindSpeedHurricane", [], 80), #12
-    ("minWindSpeedHurricane", [], 15), #13
-]
+# find student's code and import it as MAIN module
+PROGRAM = 'main.py'
+MODULE_NAME = 'main'
+if len(sys.argv) == 2:
+    PROGRAM = sys.argv[1]
+    MODULE_NAME = PROGRAM.split('.')[0]
+MAIN = importlib.import_module(MODULE_NAME)
 
-TESTS_PRINT = [
-        ["The number of records in the DataSet is ", 529], #1
-        ["The name of the hurricane in the 20th row of the dataset is ", "BABE"],
-        ["Does the hurricane 'BOB' Exist in the Dataset?", "True"],
-        ["Does the hurricane 'BRITNEY' Exist in the Dataset?", "False"],
-        ["Find the hurricane that came at 9.0N Latitude and 157.0W Longitude", "JUNE"],
-        ["How many hurricanes came from the Pacific?", 241],
-        ["How many hurricanes came from the Atlantic?", 288],
-        ["What is the difference between the max Windspeed and min Windspeed of the dataSet?", 65]
- ]
-"""TESTS_PRINT = [
-    ["The area of Wisconsin is ", 65496.38], #1
-    ["The population of Wisconsin in 2010 is ", 5686986.0], #2
-    ["Maximum land area among Wisconsin, Iowa, Minnesota is ", 86935.83], #3
-    ["Minimum population density among Wisconsin, Iowa, Minnesota in 2000 is ", 52.002450206414075], #4
-    ["The predicted population for Wisconsin in year 2010 is (assume start yearA is 2000, growth rate is 0.5) ", 796039951.1495125], #5
-    ["The growth rate for Wisconsin between year 2000 and 2010 is ", 0.005853103209551789] #6
-]"""
-#TESTS_PRINT=[]
+# find python binary
+def get_python_binary_name():
+    try:
+        subprocess.check_output(['python3', '--version'], stderr=subprocess.STDOUT)
+        return 'python3'
+    except:
+        return 'python'
 
-TESTS_FUNC_NUM = len(TESTS_FUNC)
-TESTS_PRINT_NUM = len(TESTS_PRINT)
-PROBLEMS = TESTS_FUNC_NUM + TESTS_PRINT_NUM
+def get_python_version(python_binary):
+    try:
+        output = subprocess.check_output([python_binary, '--version'],
+                                         stderr=subprocess.STDOUT)
+        output = str(output, 'utf-8')
+    except:
+        return 'unknown'
+    return output
+
+PYTHON_BINARY = get_python_binary_name()
+PYTHON_VERSION = get_python_version(PYTHON_BINARY)
+print('Your Python version: '+PYTHON_VERSION)
+
+if PYTHON_VERSION.lower().find('python 3') < 0:
+    print('WARNING! Your Python version may not work for this class.')
+    print('Please check with us about this.')
+    print()
+
+##################################################
+# Problems are based on output of student program
+##################################################
+
+def problem_1(actual_lines):
+    return check_problem(actual_lines, [
+        "The number of records in the DataSet is ",
+        529
+    ])
+
+def problem_2(actual_lines):
+    return check_problem(actual_lines, [
+        "The name of the hurricane in the 20th row of the dataset is ",
+        "BABE"
+    ])
+
+def problem_3(actual_lines):
+    return check_problem(actual_lines, [
+        "Does the hurricane 'BOB' Exist in the Dataset?",
+        "True"
+    ])
+
+def problem_4(actual_lines):
+    return check_problem(actual_lines, [
+        "Does the hurricane 'BRITNEY' Exist in the Dataset?",
+        "False"
+    ])
+
+def problem_5(actual_lines):
+    return check_problem(actual_lines, [
+        "Find the hurricane that came at 9.0N Latitude and 157.0W Longitude",
+        "JUNE"
+    ])
+
+def problem_6(actual_lines):
+    return check_problem(actual_lines, [
+        "How many hurricanes came from the Pacific?",
+        241
+    ])
+
+def problem_7(actual_lines):
+    return check_problem(actual_lines, [
+        "How many hurricanes came from the Atlantic?",
+        288
+    ])
+
+def problem_8(actual_lines):
+    return check_problem(actual_lines, [
+        "What is the difference between the max Windspeed and min Windspeed of the dataSet?",
+        65
+    ])
+
+##################################################
+# Tests are based on calling student functions
+##################################################
+
+def test_1():
+    error = check_has_function('testGetNumRecords')
+    if error:
+        return error
+    expected = 529
+    actual = MAIN.testGetNumRecords()
+    return check_answer(expected, actual)
+
+def test_2():
+    error = check_has_function('testGetName')
+    if error:
+        return error
+    actual = MAIN.testGetName(24)
+    return check_answer(expected, actual)
+
+def test_3():
+    error = check_has_function('testGetWindSpeed')
+    if error:
+        return error
+    expected = 25
+    actual = MAIN.testGetWindSpeed(38)
+    return check_answer(expected, actual)
+
+def test_4():
+    error = check_has_function('searchHurricane')
+    if error:
+        return error
+    expected = True
+    actual = MAIN.searchHurricane("EMILY")
+    return check_answer(expected, actual)
+
+def test_5():
+    error = check_has_function('searchHurricane')
+    if error:
+        return error
+    expected = False
+    actual = MAIN.searchHurricane("Random1")
+    return check_answer(expected, actual)
+
+def test_6():
+    error = check_has_function('searchHurricane')
+    if error:
+        return error
+    expected = True
+    actual = MAIN.searchHurricane("KENNETH")
+    return check_answer(expected, actual)
+
+def test_7():
+    error = check_has_function('searchHurricane')
+    if error:
+        return error
+    expected = False
+    actual = MAIN.searchHurricane("BORA")
+    return check_answer(expected, actual)
+
+def test_8():
+    error = check_has_function('searchHurricaneByLocation')
+    if error:
+        return error
+    expected = "FAITH"
+    actual = MAIN.searchHurricaneByLocation("12.9N", "20.5W")
+    return check_answer(expected, actual)
+
+def test_9():
+    error = check_has_function('searchHurricaneByLocation')
+    if error:
+        return error
+    expected = "HENRIETTE"
+    actual = MAIN.searchHurricaneByLocation("11.2N","95.0W")
+    return check_answer(expected, actual)
+
+def test_10():
+    error = check_has_function('countHurricane')
+    if error:
+        return error
+    expected = 288
+    actual = MAIN.countHurricane("Atlantic")
+    return check_answer(expected, actual)
+
+def test_12():
+    error = check_has_function('countHurricane')
+    if error:
+        return error
+    expected = 80
+    actual = MAIN.maxWindSpeedHurricane()
+    return check_answer(expected, actual)
+
+def test_13():
+    error = check_has_function('minWindSpeedHurricane')
+    if error:
+        return error
+    expected = 15
+    actual = MAIN.minWindSpeedHurricane()
+    return check_answer(expected, actual)
+
+def check_has_function(function_name):
+    for name, member in getmembers(MAIN):
+        if name == function_name and isfunction(member):
+            return None
+    return 'missing function ' + function_name
+
+def check_answer(expected, actual):
+    if expected == actual:
+        return PASS
+    else:
+        return 'expecting %s, but got %s' % (repr(expected), repr(actual))
 
 def clean_lines(lines):
     result = []
@@ -59,6 +215,7 @@ def clean_lines(lines):
 def areLinesExpected(actual_lines, expected_lines):
     actual_lines = clean_lines(actual_lines)
     expected_lines = clean_lines(expected_lines)
+
     if len(actual_lines) < len(expected_lines):
         return 'fewer output lines than expected'
     if len(actual_lines) > len(expected_lines):
@@ -77,83 +234,21 @@ def areLinesExpected(actual_lines, expected_lines):
             return 'expected ({}) but found ({})'.format(e, a)
     return None
 
-def runStringFuncTests(actual_line, expected_line):
-    if actual_line.lower() != expected_line.lower():
-        return 'expected ({}) but found ({})'.format(expected_line, actual_line)
+def check_problem(actual_lines, expected_lines):
+    error = areLinesExpected(actual_lines, expected_lines)
+    if error != None:
+        return error
     return PASS
 
-def runPrintTest(lines, expected):
-    error = areLinesExpected(lines, expected)
-    return error if error else PASS
-
-def runFuncTest(functionName, fn, args, expectedResult):
-    if fn:
-        actualResult = fn(*args)
-    else:
-        return "function %s not found" % functionName
-    if type(expectedResult) != str and abs(actualResult - expectedResult) > EPSILON:
-        return "%s(%s) expected return %s but found %s" % (
-            functionName, ', '.join([str(arg) for arg in args]), expectedResult, actualResult)
-    elif type(expectedResult) == str or type(actualResult) == str:
-        return runStringFuncTests(str(actualResult) , str(expectedResult))
-        
-    return PASS
-
-def get_python_binary_name():
+def getProblemAnswers():
     try:
-        subprocess.check_output(['python3', '--version'], stderr=subprocess.STDOUT)
-        return 'python3'
-    except:
-        return 'python'
-
-def get_python_version(python_binary):
-    try:
-        output = subprocess.check_output([python_binary, '--version'],
-                                         stderr=subprocess.STDOUT)
-        output = str(output, 'utf-8')
-    except:
-        return 'unknown'
-    return output
-
-def mainFunc():
-    result = {'score': 0, 'tests': []}
-    program = 'main.py'
-    module_name = 'main'
-    output = None
-
-    python_binary = get_python_binary_name()
-    version = get_python_version(python_binary)
-    print('Your Python version: '+version)
-
-    if version.lower().find('python 3') < 0:
-        print('WARNING! Your Python version may not work for this class.')
-        print('Please check with us about this.')
-        print()
-
-    if len(sys.argv) == 2:
-        program = sys.argv[1]
-        module_name = program.split('.')[0]
-    try:
-        cmd = [python_binary, program]
+        cmd = [PYTHON_BINARY, PROGRAM]
         print('Running your program with this command: ' + ' '.join(cmd) + '\n')
         output = subprocess.check_output(cmd)
-    except:
+    except Exception as e:
         print('Your Python program crashed.  Please fix it to get any points.')
+        raise e
 
-    main_module = importlib.import_module(module_name)
-    studentFunctions = dict([func for func in getmembers(main_module) if isfunction(func[1])])
-    for problemNum in range(1, TESTS_FUNC_NUM + 1):
-        testCase = TESTS_FUNC[problemNum - 1]
-        try:
-            testResult = runFuncTest(
-                testCase[0], studentFunctions.get(testCase[0]), testCase[1], testCase[2])
-        except Exception as e:
-            testResult = "Got exception {} when running test {}".format(
-                str(e), problemNum)
-        result['tests'].append({
-            'test': "Function test {} ({})".format(problemNum, testCase[0]),
-            'result': testResult
-        })
     problems = dict() # key:problem number, val: list of lines
     if output:
         # normalize windows => unix, as a string (not bytes)
@@ -170,21 +265,42 @@ def mainFunc():
             elif problem_number:
                 problems[problem_number].append(line)
 
-    for problemNum in range(1, TESTS_PRINT_NUM + 1):
-        testCase = TESTS_PRINT[problemNum - 1]
-        lines = problems.pop(problemNum, None)         # student code
-        # did student produce output?
-        if lines is None:
-            result['tests'].append({
-                'test': "Main function print test (Problem {})".format(problemNum),
-                'result': 'Main function print test {} output missing'.format(problemNum)
-            })
-            continue
-        testResult = runPrintTest(lines, testCase)
-        result['tests'].append({
-            'test': "Main function print test (Problem {})".format(problemNum),
-            'result': testResult
-        })
+    return problems
+
+# these call student functions
+def runTests():
+    tests = []
+    predicate = lambda f: isfunction(f) and f.__module__ == __name__
+    fns = [row for row in getmembers(sys.modules[__name__], predicate = predicate) if row[0].startswith('test_')]
+    fns.sort(key=lambda row: int(row[0].split('_')[-1]))
+    for name, fn in fns:
+        try:
+            result = fn()
+            tests.append({'test': name, 'result': result})
+        except Exception as e:
+            print('\nTip from 301 instructors: try running just %s() in interactive mode to debug this issue.\n\n' % name)
+            raise e
+    return tests
+
+# these check output from running student program
+def runProblems():
+    # collect main() output from student
+    problems = getProblemAnswers()
+    tests = []
+
+    predicate = lambda f: isfunction(f) and f.__module__ == __name__
+    fns = [row for row in getmembers(sys.modules[__name__], predicate = predicate) if row[0].startswith('problem_')]
+    fns.sort(key=lambda row: int(row[0].split('_')[-1]))
+    for name, fn in fns:
+        problem_num = int(name.split('_')[-1])
+        output_lines = problems.get(problem_num, [])
+        tests.append({'test': name, 'result': fn(output_lines)})
+    return tests
+
+def main():
+    result = {'score': 0, 'tests': []}
+    result['tests'] += runTests()
+    result['tests'] += runProblems()
 
     # final score
     passing = [t for t in result['tests'] if t['result'] == PASS]
@@ -199,4 +315,4 @@ def mainFunc():
     print('Score: %d%%' % result['score'])
 
 if __name__ == '__main__':
-    mainFunc()
+    main()
