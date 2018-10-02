80a92a348383da507c457a02fb18e0997d2e956a
Ushmal Ramesh <ushmal.ramesh@gmail.com>
2018-10-02 08:11:05 -0500

    Changed main.py, readme.md and project.py. Deleted test.py

--- fall18/p4/project.py
+++ fall18/p4/project.py
@@ -1,15 +1,35 @@
 import csv
-import pandas as pd
 
-def describe_data():
-    df = pd.read_csv('IMDB-Movie-Data.csv')
-    print(df.head().to_string(index=False))
-    print()
+def getNumRecords():
+    """This function will return the number of records in the dataset"""
+    return len(imdb_movie_data)//2
 
-def get_rows():
-    return int(len(imdb_movie_data)/2)
+def getMovieData(field, movie=None, index=None):
+    """This function will return the value of the field corresponding to either a movie or an index"""
+    if movie is not None:
+        if movie not in imdb_movie_data:
+            print("Invalid Movie Name")
+            return -1 
+        if field not in imdb_movie_data[movie]:
+            print("Invalid Field Name")
+            return -1
+        return imdb_movie_data[movie][field]
+
+    if index is not None:
+        if index not in imdb_movie_data:
+            print(index)
+            print("Invalid index")
+            return -1 
+        if field not in imdb_movie_data[index]:
+            print("Invalid Field Name")
+            return -1
+        return imdb_movie_data[index][field]
+
+    print("Please specify either the Movie Name or Rank")
+
 
 def readData():
+    """This function reads the data from the csv file"""
     imdb_movie_data = {}
     with open('IMDB-Movie-Data.csv') as csvimdb_movie_dataFile:
         csvReader = csv.reader(csvimdb_movie_dataFile)
@@ -19,32 +39,9 @@ def readData():
                 header=False
                 continue
             # key = rank
-            imdb_movie_data[int(row[0])] = {'Title':row[1],'Genres':row[2],'Description':row[3],'Director':row[4],'Cast':row[5],'Year':int(row[6]),'Runtime':int(row[7]),'Rating':float(row[8]),'Revenue':row[9]}
+            imdb_movie_data[int(row[0])] = {'Title':row[1],'Genres':row[2],'Director':row[3],'Cast':row[4],'Year':int(row[5]),'Runtime':int(row[6]),'Rating':float(row[7]),'Revenue':row[8]}
             # key = title
-            imdb_movie_data[row[1]] = {'Index':int(row[0]),'Genres':row[2],'Description':row[3],'Director':row[4],'Cast':row[5],'Year':int(row[6]),'Runtime':int(row[7]),'Rating':float(row[8]),'Revenue':row[9]}
+            imdb_movie_data[row[1]] = {'Index':int(row[0]),'Genres':row[2],'Director':row[3],'Cast':row[4],'Year':int(row[5]),'Runtime':int(row[6]),'Rating':float(row[7]),'Revenue':row[8]}
     return imdb_movie_data
-
-def getMovieData(field, MovieName=None, Index=None):
-    if MovieName is not None:
-        if MovieName not in imdb_movie_data:
-            print("Invalid Movie Name")
-            return -1 
-        if field not in imdb_movie_data[MovieName]:
-            print("Invalid Field Name")
-            return -1
-        return imdb_movie_data[MovieName][field]
-
-    if Index is not None:
-        if Index not in imdb_movie_data:
-            print(Index)
-            print("Invalid Index")
-            return -1 
-        if field not in imdb_movie_data[Index]:
-            print("Invalid Field Name")
-            return -1
-        return imdb_movie_data[Index][field]
-
-    print("Please specify either the Movie Name or Rank")
-
 
 imdb_movie_data = readData()
