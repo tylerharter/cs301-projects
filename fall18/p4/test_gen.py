4a5c4c55c2ba86fe123ad4d34fc89ca4a05e589d
tylerharter <tylerharter@gmail.com>
2018-10-03 19:55:15 -0500

    rebalance tests across topics

--- fall18/p4/test_gen.py
+++ fall18/p4/test_gen.py
@@ -15,6 +15,11 @@ MAIN = importlib.import_module(MODULE_NAME)
 
 PROMPT = 'enter 0 to 100, or a special command [q:quit, r:reset, c:count, a:average]: '
 
+
+GOOD_NUM = 'good_num'
+BAD_NUM = 'bad_num'
+GARBAGE = 'garbage'
+
 # find python binary
 def get_python_binary_name():
     try:
@@ -53,38 +58,63 @@ class TestGen:
                 lines[i] += in_lines.pop(0)
         return '\n'.join(lines)
 
-    def gen_test(self, steps=2, choices=['r', 'c', 'a', 'good_num', 'bad_num', 'garbage']):
+    def gen_test(self, steps=2, choices=['r', 'c', 'a', GOOD_NUM, BAD_NUM, GARBAGE]):
         in_lines = []
         for i in range(steps-1):
             cmd = random.choice(choices)
-            if cmd == 'good_num':
-                cmd = str(random.randint(0, 100))
-            elif cmd == 'bad_num':
+            if cmd == GOOD_NUM:
+                if random.randint(0, 4) == 0:
+                    cmd = str(random.randint(0, 100))
+                else:
+                    cmd = str(random.randint(40, 100))
+            elif cmd == BAD_NUM:
                 cmd = str(random.randint(101, 1000))
-            elif cmd == 'garbage':
-                cmd = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randint(2, 10)))
+            elif cmd == GARBAGE:
+                cmd = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(random.randint(2, 10)))
             in_lines.append(cmd)
         in_lines.append('q')
         self.traces.append(self.trace_io(in_lines))
 
-    def gen_tests(self, count=1, steps=2, choices=['r', 'c', 'a', 'good_num', 'bad_num', 'garbage']):
+    def gen_tests(self, count=1, steps=2, choices=['r', 'c', 'a', GOOD_NUM, BAD_NUM, GARBAGE]):
         for i in range(count):
             self.gen_test(steps, choices)
 
     def dump_code(self, start):
         for i, trace in enumerate(self.traces):
-            print('def test_%d():' % (i+start))
+            num = (i+start)
+            print('def test_%d():' % num)
+            print("    print('TEST %d')" % num)
             print("    return generic_test('''")
-            print(trace)
+            print(trace.strip())
             print("'''.strip())")
             print()
-        
+
 def main():
     random.seed(301)
     tests = TestGen()
-    tests.gen_tests(5, steps=3)
-    tests.gen_tests(5, steps=6)
-    tests.dump_code(start=2)
+
+    # enter one good number and give correct grade (25%)
+    tests.gen_tests(1, steps=1, choices=[])
+    tests.gen_tests(49, steps=2, choices=[GOOD_NUM])
+
+    # count, and occasionally reset (25%)
+    tests.gen_tests(40, steps=5, choices=[GOOD_NUM, 'c'])
+    tests.gen_tests(10, steps=8, choices=[GOOD_NUM, 'c', 'r'])
+
+    # average, and occasionally reset (25%)
+    tests.gen_tests(40, steps=5, choices=[GOOD_NUM, 'a'])
+    tests.gen_tests(10, steps=8, choices=[GOOD_NUM, 'a', 'r'])
+
+    # longer traces, mix of everything (10%)
+    tests.gen_tests(20, steps=10, choices=[GOOD_NUM, 'c', 'r', 'a'])
+
+    # tricky inputs (15%)
+    tests.gen_tests(6, steps=10, choices=[GOOD_NUM, 'C', 'R', 'A'])
+    tests.gen_tests(6, steps=5, choices=[GOOD_NUM, BAD_NUM])
+    tests.gen_tests(6, steps=5, choices=[GOOD_NUM, GARBAGE])
+    tests.gen_tests(6, steps=10, choices=[GOOD_NUM, BAD_NUM, GARBAGE, 'c', 'r', 'a', 'C', 'R', 'A'])
+    tests.gen_tests(6, steps=30, choices=[GOOD_NUM, BAD_NUM, GARBAGE, 'c', 'r', 'a', 'C', 'R', 'A'])
+    tests.dump_code(start=1)
 
 if __name__ == '__main__':
     main()
