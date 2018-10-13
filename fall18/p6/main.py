e7d704d19278de1da5e10eece81880bb2b5fcdce
omjb13 <2487338+omjb13@users.noreply.github.com>
2018-10-13 13:56:27 -0500

    P6 complete

--- fall18/p6/main.py
+++ fall18/p6/main.py
@@ -1,91 +1,40 @@
-def read_csv(filename):
+import sys
+
+def get_row(dataset, n):
+  if dataset == None:
+    return None
+  else:
+    return dataset[n]
+
+#### ADD YOUR FUNCTIONS HERE ####
+
+def process_args():
   """
-
-  read_csv("dataset.csv") -> [ [name, windspeed, ocean], .... ]
-
+  sys.argv[0] is the python file (in our case, it's going to be main.py)
+  sys.argv[1] is the command
+  sys.argv[2] is the name of the CSV file
+  sys.argv[3] onwards are additional parameters required by commands
+  HINT : everything in argv is going to be a string!
   """
-  pass
+  # Use the read_csv function to read in the CSV file and save it to a "dataset" variable
+  # This is also the only time you should ever call the read_csv function. All the subsequent functions MUST use the dataset variable
+  dataset = None
 
+  # define a command for each of the functions you implement
+  # we've implemented get_row for you
+  command = sys.argv[1]
+  output = None
 
-def sample(dataset, n, start_or_end):
-  """
+  if command == 'get_row':
+    # USAGE - python main.py get_row hurricanes.csv <row_index>
+    # This function is going to return None until you actually implement read_csv and save it's result in dataset
+    output = get_row(dataset, int(sys.argv[3]))
 
-  returns the first n or last n elements in dataset depending on start_or_end
-
-  """
-  pass
-
-
-def get_column(dataset, col_idx):
-  """
-
-  returns the entire column at col_idx
-
-  """
-  pass
-
-
-def windspeed_filter(rows, low, high):
-  """
-  returns rows with windspeed between low and high
-  """
-  pass
-
-
-def filter_on_col(rows, filter_on_col, filter_value):
-  """
-
-  returns rows where the value of filter_on_col == filter_value
-  --> filter_row(rows, 0, "BOB") --> only 1 row returned
-  --> filter_row(rows, 2, "Atlantic" ---> multiple rows returned
-  """
-  pass
-
+  print(output)
 
 def main():
+  process_args()
 
-  ## TODO - IMPLEMENT read_csv() BEFORE YOU DO THESE
-  hurricanes = read_csv("hurricanes.csv")
+if __name__ == '__main__':
+    main()
 
-  print("Number of hurricanes: ")
-  print()
-
-  print("The first row is: ")
-  print()
-
-  print("The 10th row is: ")
-  print()
-
-  print("What is the name of the hurricane in the 5th row? ")
-  print()
-
-  print("What is the windspeed of the hurricane in the 5th row? ")
-  print()
-
-  ## TODO - IMPLEMENT sample() BEFORE YOU DO THESE
-  print("The first 5 rows in the dataset are: ")
-  print()
-
-  print("The last 7 rows in the dataset are: ")
-  print()
-
-  ## TODO - IMPLEMENT get_column() BEFORE YOU DO THESE
-  print("The names of all the hurricanes sorted alphabetically are: ")
-  print()
-
-  print("The average windspeed of all hurricanes is : ")
-  print()
-
-  print("What was the 2nd fastest hurricane?")
-  print()
-
-  ## TODO - IMPLEMENT windspeed_filter() BEFORE YOU DO THESE
-  print("How many hurricanes had a windspeed betewen 30 and 50?")
-  print()
-
-  ## TODO - IMPLEMENT filter_on_col() BEFORE YOU DO THESE
-  print("How many hurricanes occured in the Atlantic Ocean? ")
-  print()
-
-  print("Is the average windspeed of hurricanes in the atlantic ocean > pacific?")
-  print()
