b58b8ff639aea1cc83e451055a174f8072ce0014
Frank Zou <Shenghao@zoushenghaos-MacBook-Air.local>
2018-10-18 10:28:44 -0500

    split request sender and grade fetcher

--- dockerGrader/client/dockerGraderClient.py
+++ dockerGrader/client/dockerGraderClient.py
@@ -1,4 +1,4 @@
-import argparse, boto3, botocore, csv, math, logging, requests, threading, time, json
+import argparse, boto3, botocore, csv, logging, requests, time
 
 REQUEST_URL="http://{ip}:{port}/json/{project}/{netId}"
 RESULT_PATH="ta/grading/{project}/{netId}.json"
@@ -38,10 +38,8 @@ def getNetIdList(numTasks):
     limit = min(numTasks, len(processedKeys)) if numTasks else len(processedKeys)
     return processedKeys[:limit]
 
-def sendRequests(ip, port, project, netIdList, numContainers):
-    counter = 0
+def sendRequests(ip, port, project, netIdList):
     for netId in netIdList:
-        counter += 1
         url = REQUEST_URL.format(ip=ip, port=port, project=project, netId=netId)
         logging.info("sending get request:" + url)
         response = requests.get(url)
@@ -51,10 +49,6 @@ def sendRequests(ip, port, project, netIdList, numContainers):
                     response.status_code, url
                 )
             )
-        if counter >= numContainers:
-            counter = 0
-            logging.info("wait for the completion.")
-            time.sleep(3)
 
 # General template to fetch from s3
 def s3Fetcher(path, name, raiseError = True):
@@ -70,50 +64,6 @@ def s3Fetcher(path, name, raiseError = True):
                 "Unexpected error {} when look up for {}.".format(e.response['Error']['Code'], name))
             if raiseError:
                 raise e
-
-def lookupGoogleId(netId):
-    path = 'users/net_id_to_google/%s.txt' % netId
-    return s3Fetcher(path, "Google ID", False)
-
-def getMetadataInfoJson(netId, project):
-    googleId = lookupGoogleId(netId)
-    metadataPath = CUR_SUBMISSION_PATH.format(googleId=googleId, project=project)
-    response = s3Fetcher(metadataPath, "Metadata Info", False)
-    return json.loads(response)
-
-def downloadGrade(netIdList, project):
-    gradeInfo = []
-    for netId in netIdList:
-        resultPath = RESULT_PATH.format(project=project, netId=netId)
-        response = s3Fetcher(resultPath, "Grade Result", False)
-        if not response:
-            gradeInfo.append((netId, 0, "Result not found", -1, ""))
-            continue
-
-        gradeJson = json.loads(response)
-        grade = gradeJson.get("score", 0)
-        errorReason = gradeJson.get("error")
-        partnerId = gradeJson.get("partner_netid")
-        metadataInfo = getMetadataInfoJson(netId, project)
-
-        if "score" not in gradeJson:
-            logging.warning("Invalid result. json file fetched for netId: {}".format(netId))
-        lateDays = metadataInfo.get("late_days", 0) if metadataInfo else -1
-        intLateDays = 0 if lateDays <= 0 else math.ceil(lateDays)
-        logging.info("netId: {} processed. grade: {} {} late days: {}".format(
-            netId,
-            grade,
-            "Error: {}".format(errorReason) if errorReason else "",
-            lateDays))
-        gradeInfo.append((netId, grade, errorReason, intLateDays, partnerId))
-    return gradeInfo
-
-def generateCsv(gradeInfo, saveFileName):
-    with open(saveFileName, "w") as csvfw:
-        csvWriter = csv.writer(csvfw)
-        for row in gradeInfo:
-            csvWriter.writerow(row)
-    print("Successfully generated csv file " + saveFileName)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send batch requests to dockerGrader')
@@ -134,12 +84,6 @@ if __name__ == "__main__":
         required=True,
         help='set dockerGrader server grading project')
     parser.add_argument(
-        '-n',
-        '--num_containers',
-        type=int,
-        default=2,
-        help='number of docker containers to run at the same time')
-    parser.add_argument(
         '--num_tasks',
         type=int,
         default=None,
@@ -149,11 +93,5 @@ if __name__ == "__main__":
     netIdList = getNetIdList(args.num_tasks)
     logging.info("Got {} netIds.".format(len(netIdList)))
     sendRequests(
-        args.ip, args.port, args.project, netIdList, args.num_containers)
-    print("Waiting for the completion of grading container", end="")
-    for i in range(5):
-        time.sleep(1)
-        print("...")
-    gradeInfo = downloadGrade(netIdList, args.project)
-    generateCsv(gradeInfo, "{}_{}.csv".format(
-        args.project, time.strftime("%m%d_%H%M", time.gmtime(time.time()))))
+        args.ip, args.port, args.project, netIdList)
+    print("Success!")
