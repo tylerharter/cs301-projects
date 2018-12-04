6bdd855456261e8a56ec3e3e33415f99277e6e01
Frank <frank@ip-172-31-20-178.us-east-2.compute.internal>
2018-12-04 01:55:09 +0000

    change timeout, add fake requests wrapper

--- dockerGrader/dockerUtil.py
+++ dockerGrader/dockerUtil.py
@@ -187,14 +187,14 @@ class dockerGrader:
 
         # run tests inside a docker container
         image = 'shenghaozou/301testenv:v1'
-        cmd = ['timeout', '70',                           # set a timeout
+        cmd = ['timeout', '300',                           # set a timeout
                'docker', 'run',                           # start a container
                '--rm',                                    # remove the container when exit
                '-v', os.path.abspath(self.testDir)+':/code',  # share the test dir inside
                '-u', str(self.currentUID),                     # run as local user (instead of root)
                '-w', '/code',                             # working dir is w/ code
                image,                                     # what docker image?
-               'timeout', '60',
+               'timeout', '280',
                'python3', 'test.py']                      # command to run inside
 
         try:
