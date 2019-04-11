cb6d2a0ec2033bce1122404707bba5a69a27c793
omjb13 <2487338+omjb13@users.noreply.github.com>
2019-04-11 13:45:04 -0500

    More tests added

--- linter/demo1.py
+++ linter/demo1.py
@@ -2,8 +2,19 @@ list = [1,2,3,4,5]
 s = 0
 boolean = True
 
+NO_WARNING = True
+
+def WARNING():
+    a = 10
+    return a
+
+return s
+
+s = s
+
 for X in list: 
     s += X
+    continue
 
 if s % 2 == 0:
     print("s is even") 
@@ -12,3 +23,12 @@ if boolean == True:
     print("true")
 
 print(s)
+
+def abc():
+    pass
+
+def abc():
+    a = 1
+
+def abc():
+    a = 2
